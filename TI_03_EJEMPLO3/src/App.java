7857c799f984804fb7531ca0bfe0d6dff4734553
Eliel003 <elielorgz03@gmail.com>
2025-05-24 09:26:25 -0600

    Este cambio se subio desde VS studio

---
+++ TI_03_EJEMPLO3/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("_____________________________________________");
+        System.out.println("Esta es otra forma de subir codigo a Git");
+    }
+}
